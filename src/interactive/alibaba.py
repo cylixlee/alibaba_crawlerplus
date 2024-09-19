7b0b4a1d28bf67db364c749c4d4b447c2614fb18
Cylix Lee <cylix.lee@outlook.com>
2024-09-19 19:19:31 +0800

    Cannot bypass verification by selenium.

--- src/interactive/alibaba.py
+++ src/interactive/alibaba.py
@@ -24,11 +24,57 @@ class AlibabaDetailPageBrowser(AbstractInteractiveBrowser):
         self._driver.maximize_window()
         # open detail page
         self._driver.get(offer.detail_url)
+        time.sleep(2)
+        # automation_trial = False
         while self._driver.current_url != offer.detail_url:
             # captcha detection
             if "captcha" in self._driver.page_source:
-                print("captcha! waiting for manual verification...")
+                print("(de-captcha) waiting for manual verification")
                 time.sleep(15)
+                # if automation_trial:
+                #     print("(de-captcha) waiting for manual verification...")
+                #     time.sleep(15)
+                #     continue
+
+                # retrial = 0
+                # while retrial < CONFIG["captcha"]["retry"]:
+                #     # try to automate several times.
+                #     if "captcha" not in self._driver.page_source:
+                #         break
+
+                #     try:
+                #         print("(de-captcha) trying to automate verification...")
+                #         time.sleep(3)
+                #         slider = self._driver.find_element(
+                #             By.XPATH,
+                #             CONFIG["captcha"]["slider"],
+                #         )
+                #         # hold the slider
+                #         ActionChains(self._driver).click_and_hold(slider).perform()
+                #         x = random.randint(
+                #             CONFIG["captcha"]["min-slide"],
+                #             CONFIG["captcha"]["max-slide"],
+                #         )
+                #         y = random.randint(-50, 50)
+                #         ActionChains(self._driver).move_by_offset(
+                #             x, y
+                #         ).release().perform()
+                #         retrial += 1
+                #     except NoSuchElementException:
+                #         while retrial < CONFIG["captcha"]["retry"]:
+                #             try:
+                #                 retry_button = self._driver.find_element(
+                #                     By.XPATH,
+                #                     CONFIG["captcha"]["retry-button"],
+                #                 )
+                #                 ActionChains(self._driver).click(retry_button).perform()
+                #                 retrial += 1
+                #             except NoSuchElementException:
+                #                 break
+                #         if retrial >= CONFIG["captcha"]["retry"]:
+                #             print("sorry, failed to get slider item...")
+                #             break
+                # automation_trial = True
             # wait for the browser to get prepared
             time.sleep(3)
 
